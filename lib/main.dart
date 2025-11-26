8afb0ea9636787f776a277ba0a4fdd52579a43b2
C:\Users\heppo <yokikoshimizu@gmail.com>
2025-11-27 04:47:39 +0900

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'calculator_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Calculator',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const CalculatorPage(),
+    );
+  }
+}

de4fe7fe83c1c94a841c35c9e926e168506c161a
C:\Users\heppo <yokikoshimizu@gmail.com>
2025-11-27 05:37:44 +0900

    logic途中まで実装

---
+++ lib/caluculator_logic.dart
@@ -0,0 +1,53 @@
+//電卓のロジックだけを担当するクラス（UIには依存しない）
+
+class Calculator {
+  String _display = '0';
+  String? _firstOperand;
+  String? _operator;
+  bool _shouldReset = false;
+
+  //画面に表示する文字列（読み取り専用）←読み取り専用って…？
+  String get display => _display;
+
+  //ボタンが押されたときに呼び出す入り口
+  void press(String value) {
+    if (value == 'AC') {
+      _allClear();
+    } else if (_isDigit(value) || value == '.') {
+      _inputDigitOrDot(value);
+    } else if (_iaOperand(value)) {
+      _setOperator(value);
+    } else if (Value == '=') {
+      _calculateResult();
+    }
+    //なぜここはelseが無い？
+  }
+
+  bool _isDigit(String v) => '0123456789'.contains(v);
+
+  bool _Operator(String v) => ['+', '-', '×', '÷'].contains(v);
+
+  void _allClear() {
+    _display = '0';
+    _firstOperand = null;
+    _operator = null;
+    _shouldReset = false;
+  }
+
+  void _inputDigitOrDot(String v) {
+    //小数点の重複を防ぐ
+    if (v == '.' && _display.contains('.')) return;
+
+    if (_shouldReset) {
+      //演算子の後の最小の入力
+      _display = (v == '.') ? '0.' : v;
+      _shouldReset = false;
+    } else {
+      if (_display == '0' && v != '.') {
+        _display = v; //先頭の0を置き換える
+      } else {
+        _display += v;
+      }
+    }
+  }
+}
